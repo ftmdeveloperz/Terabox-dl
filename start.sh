ad024f436aab1ffccd49c119d43a26ae55f4eaf7
FTM DEVELOPER <106005123+ftmdeveloperz@users.noreply.github.com>
2025-04-07 11:48:55 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "╭───────────────╮"
+echo "│ Fᴛᴍ Dᴇᴠᴇʟᴏᴘᴇʀᴢ │ Starting Bot..."
+echo "╰───────────────╯"
+
+# Activate virtual environment if needed
+# source venv/bin/activate
+
+# Launch the bot
+python3 ftm.py
